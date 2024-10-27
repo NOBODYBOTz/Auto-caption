dd09d1fafc7da2a424bcb165a9a4650dbdea606d
NOBODY BOTz <140589004+NOBODYBOTz@users.noreply.github.com>
2024-10-27 16:44:09 +0530

    Update Caption.py

--- Rkn_Bots/Caption.py
+++ Rkn_Bots/Caption.py
@@ -134,15 +134,20 @@ async def auto_edit_caption(bot, message):
                     re.sub(r"@\w+\s*", "", file_name)
                     .replace("_", " ")
                     .replace(".", " ")
+                    .replace("[]"," ")
                 )
+
+                # Get the caption of the file (if exists)
+                file_caption = message.caption or "No caption"
+                
                 cap_dets = await chnl_ids.find_one({"chnl_id": chnl_id})
                 try:
                     if cap_dets:
                         cap = cap_dets["caption"]
-                        replaced_caption = cap.format(file_name=file_name)
+                        replaced_caption = cap.format(file_name=file_name, file_caption=file_caption)
                         await message.edit(replaced_caption)
                     else:
-                        replaced_caption = Rkn_Bots.DEF_CAP.format(file_name=file_name)
+                        replaced_caption = Rkn_Bots.DEF_CAP.format(file_name=file_name, file_caption=file_caption)
                         await message.edit(replaced_caption)
                 except FloodWait as e:
                     await asyncio.sleep(e.x)
